0d538158414e07578b15188043521d1ebb33a36f
Gokul Ganesh I <igokulganesh@gmail.com>
2023-12-04 22:30:58 +0530

    Block Class Added

--- src/__test__/block.test.js
+++ src/__test__/block.test.js
@@ -1,18 +1,53 @@
-import Block from "../block.js";
+import Block from "../block";
+import { GENESIS_DATA } from "../config";
 
 describe("Block", () => {
-    const data = ["some-data", "random-data"];
-    const timestamp = "current-time";
-    const lasthash = "last-hash";
-    const hash = "hash";
-    const block = new Block({
-        data, 
-        timestamp,
-        lasthash,
-        hash
+  const data = "some-data";
+  const timestamp = "current-time";
+  const lastHash = "last-hash";
+  const hash = "hash";
+  const block = new Block({
+    data,
+    timestamp,
+    lastHash,
+    hash,
+  });
+
+  it("has a all the property exits", () => {
+    expect(block.data).toEqual(data);
+    expect(block.timestamp).toEqual(timestamp);
+    expect(block.lastHash).toEqual(lastHash);
+    expect(block.hash).toEqual(hash);
+  });
+
+  describe("Genesis", () => {
+    const genesis_block = Block.genesis();
+
+    it("returns a Block Instance", () => {
+      expect(genesis_block instanceof Block).toBe(true);
     });
-    
-    it("has a all the property exits", () => {
-        expect(block.timestamp).toEqual(timestamp);
-    }); 
+
+    it("returns the genesis data", () => {
+      expect(genesis_block).toEqual(GENESIS_DATA);
+    });
+  });
+
+  describe("mineBlock()", () => {
+    const lastBlock = Block.genesis();
+    const data = "mined-data";
+    const minedBlock = Block.mineBlock({ lastBlock, data });
+
+    it("returns a Block Instance", () => {
+      expect(minedBlock instanceof Block).toBe(true);
+    });
+
+    it("sets the `lastHash` to be the `hash` of the lastBlock", () => {
+      expect(minedBlock.lastHash).toEqual(lastBlock.hash);
+    });
+
+    it("sets the `data` and `timestamp`", () => {
+      expect(minedBlock.data).toEqual(data);
+      expect(minedBlock.timestamp).not.toEqual(undefined);
+    });
+  });
 });
--- src/block.js
+++ src/block.js
@@ -1,9 +1,19 @@
+import { GENESIS_DATA } from "./config";
+
 class Block {
   constructor({ data, timestamp, lastHash, hash }) {
     this.data = data;
     this.timestamp = timestamp;
     this.lastHash = lastHash;
     this.hash = hash;
+  }
+
+  static mineBlock({ lastBlock, data }) {
+    return new this({ data, timestamp: Date.now(), lastHash: lastBlock.hash });
+  }
+
+  static genesis() {
+    return new this(GENESIS_DATA);
   }
 }
 
