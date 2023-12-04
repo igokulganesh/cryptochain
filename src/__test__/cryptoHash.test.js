6195db4b49f9509690818cfa7d5f514e8dfe6db6
Gokul Ganesh I <igokulganesh@gmail.com>
2023-12-04 22:42:11 +0530

    cryptoHash SHA256 Method added

---
+++ src/__test__/cryptoHash.test.js
@@ -0,0 +1,15 @@
+import { cryptoHash } from "../cryptoHash";
+
+describe("cryptoHash()", () => {
+  it("generate a SHA-256 Hashed output", () => {
+    expect(cryptoHash("gokul")).toEqual(
+      "2ce6db3d86b23e69ce4e4c892ffbe5121a0fedb7ccbb6317df15e9362cada4ba"
+    );
+  });
+
+  it("produces the same hash with same input arguments in any order", () => {
+    expect(cryptoHash("one", "two", "three")).toEqual(
+      cryptoHash("three", "two", "one")
+    );
+  });
+});
